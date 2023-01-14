e27482a683486fc1b5a4ce1c99c7379beacc4e59
Pushpajit Biswas <pushpajitnexus007@gmail.com>
2023-01-14 14:56:20 +0530

    Lexicographically Smallest Equivalent String problem and link added

---
+++ Leetcode/Lexicographically_Smallest_Equivalent_String.cpp
@@ -0,0 +1,51 @@
+/*
+    leetcode:
+        https://leetcode.com/problems/lexicographically-smallest-equivalent-string/description/
+
+*/
+
+class Solution {
+    char dfs(unordered_map<char, vector<char>> &adj, char curr, char parent, vector<int> &vis) {
+        // Initialize current node as minimum.
+
+        char MIN = curr;
+        // Mark current node as visited.
+        vis[curr - 'a'] = true;
+
+        // Simple dfs call for all neighbour.
+        for (auto node : adj[curr]) {
+
+            if (!vis[node - 'a'])
+                MIN = min(MIN, dfs(adj, node, curr, vis));
+
+        }
+
+        return MIN;
+    }
+
+public:
+    string smallestEquivalentString(string &s1, string &s2, string &baseStr) {
+        int n = s1.size();
+        string ans = "";
+        unordered_map<char, vector<char>> adj;
+
+        // Making adj list.
+        for (int i = 0; i < n; i++) {
+            adj[s1[i]].push_back(s2[i]);
+            adj[s2[i]].push_back(s1[i]);
+        }
+
+
+        // Go thouch every char of baseStr and find the minimum for that char.
+        for (int i = 0; i < baseStr.size(); i++) {
+            // visited array of size 26 for a-z
+            vector<int> vis(26);
+
+            // Add minimum char to the answer.
+            ans += dfs(adj, baseStr[i], '$', vis);
+        }
+
+        return ans;
+
+    }
+};
